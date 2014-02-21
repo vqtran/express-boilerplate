839d8f6e31ed74a061f270df4c72e73ff0641a17
Vinh Tran <vinh_tran@brown.edu>
2014-02-21 15:28:53 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-express-boilerplate
-===================
+Express Boilerplate
+=======
 
-Express configured with a Rails-style asset-pipeline.
+Express web server preconfigured with a Rails style asset pipeline.
+
+Uses LESS, and example shown using Bootstrap. 
+
+Assets from `assets/` are compiled to minified/compressed LESS/JS in `public/assets`. Templating done with EJS. `ejs-locals` provides layout support.
+
+# Usage
+
+    git clone git@github.com:vqtran/express-boilerplate.git
+    cd express-boilerplate
+    npm install
+    node server.js
+
+# Reference
+
+## Primary
+    - [Express](expressjs.com)
+    - [Bootstrap 3](getbootstrap.com)
+    - [connect-assets](https://github.com/adunkman/connect-assets)
+    - [ejs](https://github.com/visionmedia/ejs)
+    - [ejs-locals](https://github.com/RandomEtc/ejs-locals)
+
+## Relevant
+    - [uglify-js](https://github.com/mishoo/UglifyJS)
+    - [csso](https://github.com/css/csso)
+
+
+
+
+
+
+
---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var engine = require('ejs-locals');
+var assets = require('connect-assets');
+var error = require('./app/util/error.js');
+var fs = require('fs');
+
+var app = express();
+
+app.configure(function () {
+    var config = {
+        paths: ['assets/stylesheets', 'assets/javascripts'],
+        buildDir: 'public/assets',
+        build: { dev: true, prod: true },
+        compress: { dev: true, prod: true }
+    };
+    app.use(assets(config));
+    app.engine('ejs', engine);
+    app.set('view engine', 'ejs');
+    app.use('/', express.static(__dirname + '/public'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(app.router);
+    app.use(error.logErrors);
+    app.use(error.clientErrorHandler);
+    app.use(error.errorHandler);
+});
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('Listening on port' + port);
